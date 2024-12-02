204be5feca1dda7352bf1124cbce1e1aa0b7ec0e
AusteId <auste.idaite@gmail.com>
2024-12-02 16:43:23 +0400

    Add style to WelcomePage component

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -47,19 +47,42 @@ function WelcomePage() {
   return (
     <main>
       <section>
-        <h1>Knygų sąrašas</h1>
-        <section>
+        <h1 className="font-bold text-stone-300 text-2xl text-center py-5">
+          Knygų sąrašas
+        </h1>
+        <section className="flex flex-wrap g-3 justify-center">
           {books.map((book) => (
-            <div key={book.id}>
-              <img src={book.cover} alt={book.title} />
-              <h2>{book.title}</h2>
-              <p>{book.author}</p>
-              <p>{book.category}</p>
-              <p>{book.price}€</p>
-              <button onClick={() => reserveHandler(book.id, book.reserved)}>
-                {book.reserved ? "Grąžinti" : "Išduoti skaitytojui"}
-              </button>
-              <button onClick={() => setEditBook(book)}>Redaguoti</button>
+            <div
+              key={book.id}
+              className="m-5 p-3 border-2 flex flex-col items-center rounded-3xl"
+            >
+              <img
+                src={book.cover}
+                alt={book.title}
+                className="w-32 object-cover"
+              />
+              <h2 className="text-stone-300 font-bold pt-3 text-lg truncate">
+                {book.title}
+              </h2>
+              <p className="text-stone-300 font-semibold truncate">
+                {book.author}
+              </p>
+              <p className="text-stone-300">{book.category}</p>
+              <p className="text-stone-300">{book.price}€</p>
+              <div>
+                <button
+                  className="text-stone-300 btn btn-ghost"
+                  onClick={() => reserveHandler(book.id, book.reserved)}
+                >
+                  {book.reserved ? "Grąžinti" : "Išduoti skaitytojui"}
+                </button>
+                <button
+                  className="text-stone-300 btn btn-ghost"
+                  onClick={() => setEditBook(book)}
+                >
+                  Redaguoti
+                </button>
+              </div>
             </div>
           ))}
         </section>
